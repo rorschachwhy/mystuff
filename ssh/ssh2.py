641d635ebb00a7bad792403b2cd0574c6f63c55a
wanghaiyang <wanghaiyang@i360r.com>
2016-05-27 14:32:01 +0800

    【王海洋】完成选择环境执行命令（支持模糊匹配）

--- ssh/ssh2.py
+++ ssh/ssh2.py
@@ -2,39 +2,40 @@
 
 import threading
 import paramiko
+import re
 
 hostnames = {
     # t1
-    "t101": "58.68.148.50",
+    # "t101": "58.68.148.50",
     "t102": "58.68.148.51",
     "t111": "58.68.148.52",
     "t112": "58.68.148.53",
     # t2
-    "t201": "58.68.233.90",
+    # "t201": "58.68.233.90",
     "t202": "58.68.233.91",
     "t211": "58.68.233.92",
     "t212": "58.68.233.93",
     # t3
-    "t301": "123.56.16.9",
+    # "t301": "123.56.16.9",
     "t302": "123.56.16.14",
     "t311": "123.56.16.19",
     "t312": "123.56.16.24",
     # t4
-    "t401": "58.68.148.59",
+    # "t401": "58.68.148.59",
     "t402": "58.68.148.60",
     "t411": "58.68.148.61",
     # t5
-    "t501": "58.68.224.154",
+    # "t501": "58.68.224.154",
     "t502": "58.68.224.155",
     "t511": "58.68.224.156",
     # t6
-    "t601": "58.68.148.57",
+    # "t601": "58.68.148.57",
     "t602": "58.68.148.58",
     "t611": "58.68.233.94",
 }
 
 
-def ssh(ip, username, passwd, cmd):
+def ssh(envnm, ip, username, passwd, cmd):
     try:
         myClent = paramiko.SSHClient()
         myClent.set_missing_host_key_policy(paramiko.AutoAddPolicy())
@@ -44,24 +45,37 @@ def ssh(ip, username, passwd, cmd):
             out = stdout.readlines()
             for o in out:
                 print(o)
-        print('%s\tOK\n' % ip)
+        print('%s\t%s\tOK\n' % (envnm, ip))
         myClent.close()
     except:
-        print('%s\tERROR\n' % ip)
+        print('%s\t%s\tERROR\n' % (envnm, ip))
 
 if __name__ == '__main__':
-    CMD = ['rm -r test526', '']
+
     USERNAME = 'shbj'
     PASSWORD = 'shbj123'
+    CMD = ['rm -r test526', '']
+
     print("Begin....")
-    # env = input("Enter env")
+    var = input("Enter env:\n")
+    if var == 'all':
+        envs = ['t']
+    else:
+        envs = var.split()
+
     threads = []
-    for hostname in hostnames.values():
-        s = threading.Thread(target=ssh, args=(
-            hostname, USERNAME, PASSWORD, CMD))
-        threads.append(s)
+    for (envnm, ip) in hostnames.items():
+        for env in envs:
+            p = re.compile(env)
+            if(p.search(envnm)):
+                # print(env, envnm, ip)
+                s = threading.Thread(target=ssh, args=(
+                    envnm, ip, USERNAME, PASSWORD, CMD))
+                threads.append(s)
+
     for t in threads:
         t.start()
     for t in threads:
         t.join()
+    print('%s environment has been changed.\n' % len(threads))
     var = input("Press any key to continue...")
---
+++ ssh/test.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+var = input("Please input env:\n")
+l = var.split()
+print(var, l)
